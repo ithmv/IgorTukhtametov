d5c3262187d3edbf8cdc7bba39b772f78d8bb9ac
ithmv <102490549+ithmv@users.noreply.github.com>
2022-04-18 12:51:32 +0700

    webpack

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,6 @@
 import $ from 'jquery'; 
 import { OrderForm } from './forms/order-form';
 
-/* Open */
-function openNav() {
-  document.getElementById("myNav").style.height = "100%";
-}
-
-
-/* Close */
-function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
-}
-
-
-
 function init() {
     $('.portfolio__works').slick({
         slidesToShow: 4,
