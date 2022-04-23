30765a4a49559a4b96a5b79244607f763f5ecd42
ithmv <102490549+ithmv@users.noreply.github.com>
2022-04-24 03:42:20 +0700

    test

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("myNav").style.height = "60%";
-}
-function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-}
-
--- src/js/tabs.js
+++ src/js/tabs.js
@@ -46,5 +46,3 @@ class ItcTabs {
     }
   }
   new ItcTabs('.tabs');
-
-  
